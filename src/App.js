b74476102120cc3f34584efbcd7d15a083b89373
Martin Wang <shenquan.wang@sjsu.edu>
2023-01-14 20:32:26 -0800

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CS5610 Sec6
+          <br/>
+          Assignment 1
+          <br/>
+          Martin (Shenquan) Wang
         </p>
         <a
           className="App-link"
